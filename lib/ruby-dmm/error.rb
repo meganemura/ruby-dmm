8f7972cafc50ded2e8f2a3872da9aec41a1c827c
meganemura <mura2megane@gmail.com>
2014-09-13 10:16:11 +0900

    Refactor error classes

--- lib/ruby-dmm/error.rb
+++ lib/ruby-dmm/error.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 module DMM
-  class Error < StandardError; end
-  class BadRequest          < Error; end
-  class Unauthorized        < Error; end
-  class Forbidden           < Error; end
-  class NotFound            < Error; end
-  class NotAcceptable       < Error; end
-  class UnprocessableEntity < Error; end
-  class InternalServerError < Error; end
-  class NotImplemented      < Error; end
-  class BadGateway          < Error; end
-  class ServiceUnavailable  < Error; end
+  Error               = Class.new(StandardError)
+  BadRequest          = Class.new(Error)
+  Unauthorized        = Class.new(Error)
+  Forbidden           = Class.new(Error)
+  NotFound            = Class.new(Error)
+  NotAcceptable       = Class.new(Error)
+  UnprocessableEntity = Class.new(Error)
+  InternalServerError = Class.new(Error)
+  NotImplemented      = Class.new(Error)
+  BadGateway          = Class.new(Error)
+  ServiceUnavailable  = Class.new(Error)
 end
